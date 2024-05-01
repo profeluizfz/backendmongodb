5d8962029112aeec059765af1ad4289b38f67bed
profeluisfz <profeluisfz@gmail.com>
2024-04-30 19:11:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+var cors = require('cors')
+const mongoose = require('mongoose');
+require('dotenv').config(); // Manejo de variables de entorno en el archivo .env
+// Importar el archivo de las rutas
+const userRoutes = require('./routes/user')
+
+const app = express();
+const port = process.env.PORT || 3200
+
+// Middleware para json
+app.use(express.json())
+app.use(express.urlencoded({ extended:true}))
+// Middleware para rutas
+app.use('/api', userRoutes);
+// cors
+app.use(cors())
+
+app.get('/',(req, res)=>{
+    res.send("Hola, desde API REST")
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>console.log("Conectado a la base de datos de MongoDB Atlas"))
+    .catch(()=> console.error("Error de conexión a la base de datos") )
+
+app.listen(port, ()=>{
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+})
+
---
+++ model/user.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    fullname:{
+        type: String,
+        required: true
+    },
+     email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true
+    }
+
+});
+
+module.exports = mongoose.model('User', userSchema);	
